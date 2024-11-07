f618407a3ff8224d591237fb73b399ae3957fbbd
pkino <pkino@pkino.net>
2024-11-07 16:45:04 +0900

    A11をbsearch_indexを使ったコードに書き換えた

--- rb/ch03/A11.rb
+++ rb/ch03/A11.rb
@@ -1,15 +1,4 @@
-N, X = gets.chomp.split.map(&:to_i)
+_, X = gets.chomp.split.map(&:to_i)
 AS = gets.chomp.split.map(&:to_i)
-
-def solve(l, r)
-  cur = (l + r) / 2
-  if AS[cur] == X
-    cur
-  elsif AS[cur] > X
-    solve(l, cur - 1)
-  else
-    solve(cur + 1, r)
-  end
-end
-
-puts(solve(0, N - 1) + 1)
+answer = AS.bsearch_index { _1 >= X } + 1
+puts(answer)
